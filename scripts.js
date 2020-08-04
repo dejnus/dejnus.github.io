3679bf8185d587e323ed32e739d6b5c65c49d5cc
dejnus <adejnak@gmail.com>
2020-08-04 10:48:22 +0200

    button support

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function scrollToForm(){
+    var el = document.getElementById("wrapper-end");
+    el.scrollIntoView({ behavior: 'smooth'});
+}
+function scrollToWho(){
+    var el = document.getElementById("wrapper-who");
+    el.scrollIntoView({ behavior: 'smooth'});
+}
